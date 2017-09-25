c8d6e868f4d3ae138de652412981490b337da722
e5ash <e5ash.bro@gmail.com>
2017-09-25 09:31:38 +0600

    Очистка scripts.js

--- app/template/scripts/common.js
+++ app/template/scripts/common.js
@@ -1,18 +1,5 @@
 $(document).ready(function($) {
 
-	function valueElementForm(nameElement) {
-		var newNameElement = '.' + nameElement;
-			element = $(newNameElement);
-		element.each(function(index, el) {
-			var elementInput = $(this).find($(newNameElement + '__input')),
-				elementLabel = $(this).find($(newNameElement + '__label')),
-				elementValue = index + 1;
-			elementInput.attr('id', nameElement + '-' + elementValue);
-			elementLabel.attr('id', nameElement + '-' + elementValue);
-		});
-		
-	}
-	valueElementForm('checkbox');
-	valueElementForm('radio');
-
+	
+	
 });
